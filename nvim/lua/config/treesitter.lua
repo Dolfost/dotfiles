3317dcf9e641a907389a381bf2c98b7bd0e11106
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-06-14 10:16:50 +0300

    Replace "tex" to "latex" in tresitter.lua

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -10,7 +10,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "tex" },
+  ignore_install = { "latex" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -22,7 +22,7 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = { "tex", "gitcommit", "markdown"},
+    disable = { "latex", "gitcommit", "markdown"},
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
