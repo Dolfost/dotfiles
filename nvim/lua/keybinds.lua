c1351c9d49a1f4411fbdef265b12920cfa9c426e
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-08-04 13:03:19 +0300

    Divide all in sub-lua files

---
+++ nvim/lua/keybinds.lua
@@ -0,0 +1,10 @@
+-- This is a global keybinds file. The plugin-specific keybinds live in lua/plugin.lua or ftplugin/
+
+-- NO ARROW KEYS, U MFCKER! heheheha!!1!
+vim.keymap.set('n', '<Up>', '<Nop>')
+vim.keymap.set('n', '<Down>', '<Nop>')
+vim.keymap.set('n', '<Left>', '<Nop>')
+vim.keymap.set('n', '<Right>', '<Nop>')
+
+-- Regenerate ctags file with F5
+vim.keymap.set('n', '<f5>', ':!ctags -R<CR>')
---
+++ nvim/lua/options.lua
@@ -0,0 +1,44 @@
+vim.o.compatible = false
+
+vim.cmd("filetype plugin indent on") -- VimTex requirement
+
+vim.o.encoding = "utf8" -- VimTex requirement
+vim.o.syntax = true     -- VimTex requirement
+
+vim.cmd.set("t_Co=256")
+vim.o.wildmenu = true
+vim.o.wildmode = "full"
+vim.o.history =200
+vim.o.fixendofline = false
+
+vim.o.incsearch = true
+
+vim.o.infercase = true
+vim.o.ignorecase = true
+
+vim.o.mouse = 'a'
+vim.o.wrap = true
+vim.o.number = true
+vim.o.guicursor = 'i:block-nCursor'
+vim.o.cursorline = true
+vim.o.showmode = false
+vim.o.scrolloff = 8
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- Replace grep with ripgrep
+vim.o.grepprg = 'rg\\ $*\\ --column\\ --no-heading'
+vim.o.grepformat = '%f:%l:%c%m,%l:%c%m'
+
+-- Next map  makes the :edit %:h<Tab>
+-- equivalent to       :edit %%<Tab>
+-- vim.cmd("cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
+
+
+-- Ukrainian language support
+vim.o.keymap = 'ukrainian-jcuken'
+vim.o.iminsert = 0
+vim.o.imsearch = -1
+vim.o.spelllang = 'uk,en_us,ru_ru'
+
