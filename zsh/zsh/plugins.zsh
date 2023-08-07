bc67fb287f4211d20b4f965ad13a4b89b71ad1e8
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-08-07 14:33:30 +0300

    Add new plugins

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -6,6 +6,10 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#616161, bg=None, bold, underline"
 zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
+zplug "plugins/sudo", from:oh-my-zsh
+zplug "plugins/web-search", from:oh-my-zsh
+zplug "plugins/dirhistory", from:oh-my-zsh
+
 
 zplug load
 zplug list
