b22f0c67de63f6e58faf99ec3d2dd708bbf33117
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-08-13 13:07:50 +0300

    Make zsh suggestions case insensitive

--- zsh/zsh/visuals.zsh
+++ zsh/zsh/visuals.zsh
@@ -2,4 +2,6 @@ eval `dircolors $ZSH_CONFIG/dir_colors`
 
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 export VISUAL='nvim'
