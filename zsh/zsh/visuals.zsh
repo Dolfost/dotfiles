67f5895e51c754be5d846393d8f5343a85a8e4bf
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-08-19 11:18:18 +0300

    Add man colors

--- zsh/zsh/visuals.zsh
+++ zsh/zsh/visuals.zsh
@@ -4,4 +4,7 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
+export MANPAGER="less --RAW-CONTROL-CHARS --use-color --color=d+g --color=u+y"
+export MANROFFOPT="-c"
+
 export VISUAL='nvim'
