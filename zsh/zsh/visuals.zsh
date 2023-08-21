2f89e2fc77c15377e26bb3bf6af88cef09cc61e7
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-08-21 19:06:24 +0300

    Change the option suggestions separator

--- zsh/zsh/visuals.zsh
+++ zsh/zsh/visuals.zsh
@@ -1,8 +1,8 @@
 eval `dircolors $ZSH_CONFIG/dir_colors`
 
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' list-separator '-->'
 
 export MANPAGER='less --RAW-CONTROL-CHARS --use-color --color=d+g --color=u+y'
 export MANROFFOPT="-c"
