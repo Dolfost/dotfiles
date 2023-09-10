9544656b405d4fbbae8b1206a907311d0fdf5f89
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-09-10 22:32:40 +0300

    Fix Inverse search in skim

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,17 +1,17 @@
 vim.opt_local.spell = true
 
 -- VimTex -> Skim -> nvim link
-function write_server_name()
-  local nvim_server_file = win32 and vim.cmd("$TEMP") or '/tmp' .. '/vimtexserver.txt'
---  `rm /tmp/vimtexserver.txt`
-  vim.f("writefile([v:servername], nvim_server_file)")
-end
+-- function write_server_name()
+--   local nvim_server_file = win32 and vim.cmd("$TEMP") or '/tmp' .. '/vimtexserver.txt'
+-- --  `rm /tmp/vimtexserver.txt`
+--   vim.f("writefile([v:servername], nvim_server_file)")
+-- end
 
-vim.api.nvim_create_augroup('vimtex_common', {clear = true})
-vim.api.nvim_create_autocmd({"FileType"}, {
-	pattern = "*.tex",
-	group = "vimtex_common",
-	command = "call write_server_name"})
+-- vim.api.nvim_create_augroup('vimtex_common', {clear = true})
+-- vim.api.nvim_create_autocmd({"FileType"}, {
+-- 	pattern = "*.tex",
+-- 	group = "vimtex_common",
+-- 	command = "call write_server_name"})
 
 
 vim.cmd("set suffixesadd+=.tex")
@@ -28,9 +28,9 @@ vim.g.vimtex_view_skim_sync = 1
 -- see ":help vimtex-compiler".
 vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.vimtex_compiler_latexmk = {
-    build_dir = '',
+    build_dir = 'buiddir',
     callback = 1,
-    continuous = 1,
+    continuous = 0,
     executable = 'latexmk',
     hooks = {},
     options = {
@@ -60,9 +60,5 @@ vim.g.vimtex_quickfix_open_on_warning = 0
 
 vim.g.vimtex_doc_handlers = {'vimtex#doc#handlers#texdoc'}
 vim.g.vimtex_complete_close_braces = 1
-
--- Most VimTeX mappings rely on localleader and this can be changed with the
--- following line. The default is usually fine and is the symbol "\".
-local maplocalleader = '\\'
 
 vim.keymap.set("n", "<localleader>ll", "<Cmd>update<CR><Cmd>VimtexCompile<CR>")
