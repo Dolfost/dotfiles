412f6811a27376a0a419ea75b530f13cf1396c9d
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-12-06 13:39:21 +0200

    Disable vim keys tab menu navigation

--- zsh/zsh/binds.zsh
+++ zsh/zsh/binds.zsh
@@ -2,10 +2,11 @@ bindkey -v
 bindkey "^H" backward-delete-char
 bindkey "^?" backward-delete-char
 
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
+# tab suggestions vim navigation
+# bindkey -M menuselect 'h' vi-backward-char
+# bindkey -M menuselect 'k' vi-up-line-or-history
+# bindkey -M menuselect 'l' vi-forward-char
+# bindkey -M menuselect 'j' vi-down-line-or-history
 
 bindkey ' ' magic-space
 bindkey -M menuselect '\C-o' accept-and-menu-complete
