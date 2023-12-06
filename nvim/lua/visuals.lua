fc019ea29ced20ace9ece2e17b77e40a865df69e
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-12-06 13:37:41 +0200

    Overwrite wwarning and error colors

--- nvim/lua/visuals.lua
+++ nvim/lua/visuals.lua
@@ -1,2 +1,5 @@
 vim.o.background = 'dark'
 vim.cmd.colorscheme 'PaperColor'
+
+vim.cmd([[highlight DiagnosticError gui=NONE guibg=NONE guifg=Red]])
+vim.cmd([[highlight DiagnosticWarning gui=NONE guibg=NONE guifg=Orange]])
