d09f67c951f8de1d02a3f2492b843822aeffb198
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-06 21:38:31 +0200

    Fix <Tab> luasnip expansion

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -1,4 +1,11 @@
 local cmp = require'cmp'
+local ls = require'luasnip'
+
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
 	snippet = {
@@ -27,8 +34,8 @@ cmp.setup({
 				cmp.select_next_item()
 				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
 				-- that way you will only jump inside the snippet region
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
+			elseif ls.expand_or_jumpable() then
+				ls.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -38,8 +45,8 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+			elseif ls.jumpable(-1) then
+				ls.jump(-1)
 			else
 				fallback()
 			end
