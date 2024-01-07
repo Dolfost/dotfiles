199a821d2a44377c02bf89dbf4e6088e9a103c6d
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-07 13:59:40 +0200

    Add Vim Tmux Navigator plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -69,6 +69,23 @@ local plugins = {
 	'godlygeek/tabular',
 	'ludovicchabant/vim-gutentags',
 
+	{"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+
 	{'preservim/vim-markdown',
 		ft = 'markdown',
 	},
