f0aef1aba0e0dd8c03e235cbf175c5c96496e8cc
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-10 04:18:43 +0200

    Fix path comletion, add C and Lua LSP functionality

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,28 +4,116 @@ local has_words_before = function()
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-
 return {
+	{
+		"williamboman/mason.nvim",
+		config = true,
+	},
+
+	{
+		'williamboman/mason-lspconfig.nvim',
+
+
+		opts = {
+			ensure_installed = {
+				'lua_ls',
+				'clangd',
+				'omnisharp',
+			},
+		},
+	},
+
+	{
+		'neovim/nvim-lspconfig',
+
+
+		config = function(_, opts)
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lspconfig = require("lspconfig")
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set('n', '<leader>wl', function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+					vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+					vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+					vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts) vim.keymap.set('n', '<leader>f', function()
+						vim.lsp.buf.format { async = true }
+					end, opts)
+				end,
+			})
+
+			lspconfig.lua_ls.setup {
+				capabilities = capabilities,
+			}
+			lspconfig.clangd.setup {
+				capabilities = capabilities,
+				filetypes = { "c", "cpp", "h", "hpp", "objc", "objcpp", "cuda", "proto" }
+			}
+			lspconfig.omnisharp.setup {
+				capabilities = capabilities,
+				cmd = { "dotnet", "/Users/vladyslav/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+			}		end,
+	},
+
+	{
+		'L3MON4D3/LuaSnip',
+		-- follow latest release.
+		version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = 'make install_jsregexp',
+		dependencies = { 'rafamadriz/friendly-snippets' },
+
+
+		config = function()
+			local ls = require 'luasnip'
+			require('luasnip.loaders.from_lua').load({ paths = '~/.config/nvim/LuaSnip/' })
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+
+
 	{
 		'hrsh7th/nvim-cmp',
 
 		dependencies = {
 			'neovim/nvim-lspconfig',
+			"williamboman/mason.nvim",
+			'williamboman/mason-lspconfig.nvim',
+			'onsails/lspkind.nvim',
+
 			'hrsh7th/cmp-cmdline',
 			'FelipeLema/cmp-async-path',
 			'hrsh7th/cmp-buffer',
 			'hrsh7th/cmp-nvim-lsp',
-			'onsails/lspkind.nvim',
+			'saadparwaiz1/cmp_luasnip',
 			'tamago324/cmp-zsh',
-			{'micangl/cmp-vimtex', ft = 'tex'},
-			{'hrsh7th/cmp-nvim-lua', ft = 'lua'},
+			'petertriho/cmp-git',
+			{ 'micangl/cmp-vimtex',   ft = 'tex' },
+			{ 'hrsh7th/cmp-nvim-lua', ft = 'lua' },
 		},
 
 
 		config = function()
-			local cmp = require'cmp'
-			local ls = require'luasnip'
-			local lspkind = require'lspkind'
+			local cmp = require 'cmp'
+			local ls = require 'luasnip'
+			local lspkind = require 'lspkind'
 
 			cmp.setup({
 				snippet = {
@@ -53,9 +141,9 @@ return {
 
 					-- LuaSnip support
 					['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-							-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+						if cmp.visible() then
+							cmp.select_next_item()
+							-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
 							-- that way you will only jump inside the snippet region
 						elseif ls.expand_or_jumpable() then
 							ls.expand_or_jump()
@@ -64,20 +152,20 @@ return {
 						else
 							fallback()
 						end
-						end, { 'i', 's' }),
+					end, { 'i', 's' }),
 					['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
+						if cmp.visible() then
+							cmp.select_prev_item()
 						elseif ls.jumpable(-1) then
 							ls.jump(-1)
 						else
 							fallback()
 						end
-						end, { 'i', 's' }),
+					end, { 'i', 's' }),
 				}),
 
 				formatting = {
-					fields = {'abbr', 'kind', 'menu'},
+					fields = { 'abbr', 'kind', 'menu' },
 					format = lspkind.cmp_format({
 						mode = 'text_symbol',
 						-- menu = {
@@ -92,45 +180,45 @@ return {
 				},
 
 				sources = cmp.config.sources({
-					{name = 'nvim_lsp'},
-					{name = 'luasnip'},
-					{name = 'buffer'},
-					{name = 'path'},
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+					{ name = 'async_path' },
+					{ name = 'buffer' },
 				}),
 			})
 
 			cmp.setup.filetype('gitcommit', {
 				sources = cmp.config.sources({
-					{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-					}, {
-					{ name = 'buffer' },
-				})
+					{ name = 'git',    group_index = 1 },
+					{ name = 'buffer', group_index = 2 },
+					{ name = 'async_path', group_index = 2 },
+				}),
 			})
 
 			cmp.setup.cmdline({ '/', '?' }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-				{ name = 'buffer' }
+					{ name = 'buffer' }
 				}
 			})
 
 			cmp.setup.filetype("tex", {
 				sources = {
-					{name = 'vimtex'},
-				{name = 'nvim_lsp'},
-					{name = 'luasnip'},
-					{name = 'buffer'},
-					{name = 'path'},
+					{ name = 'vimtex' },
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+					{ name = 'async_path' },
+					{ name = 'buffer' },
 				},
 			})
 
 			cmp.setup.filetype("lua", {
 				sources = {
-				{name = 'nvim_lsp'},
-				{name = 'nvim_lua'},
-					{name = 'luasnip'},
-					{name = 'buffer'},
-					{name = 'path'},
+					{ name = 'nvim_lua' },
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+					{ name = 'async_path' },
+					{ name = 'buffer' },
 				},
 			})
 
@@ -138,24 +226,16 @@ return {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
 					-- { name = 'path' },
-					{ name = 'zsh' },
-					}, {
-						{
-							name = 'cmdline',
-							option = {
-								ignore_cmds = { 'Man', '!' }
-							}
-						}
+					{
+						name = 'cmdline',
+						group_index = 1,
+						option = {
+							ignore_cmds = { 'Man', '!' }
+						},
+					},
+					{ name = 'zsh', group_index = 2 },
 				})
 			})
-
-
-			-- Set up lspconfig.
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-			require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-				capabilities = capabilities
-			}
 		end,
 	}
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,28 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', 
+		'nvim-telescope/telescope.nvim',
 
 		tag = '0.1.5',
-		dependencies = {'nvim-lua/plenary.nvim'},
 		cmd = 'Telescope',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope-ui-select.nvim',
+		},
+
+
+		config = function()
+			local telescope = require("telescope")
+
+			local opts = {
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown{},
+					},
+				},
+			}
+
+			telescope.setup(opts)
+			telescope.load_extension('ui-select')
+		end,
 	},
 }
