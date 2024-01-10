3d36fb94125903d6621dbcd812f4fc205412a105
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-10 22:29:08 +0200

    Add oil.nvim keymap and telescope keymaps description

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -11,7 +11,10 @@ vim.keymap.set('n', '<f5>', ':!ctags -R<CR>')
 
 -- Telescope keybinds
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Telescope find files"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Telescope live grep"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Telescope buffers"})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope help tags" })
+
+-- oil.nvim keybinds
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
