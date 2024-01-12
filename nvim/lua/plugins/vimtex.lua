0fb71ab93322f4226ed66e9115bd8661c358b66b
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-12 17:09:22 +0200

    Add keymaps with desc key so they will have nice names in which-key plugin

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -14,6 +14,7 @@ return {
 
 	config = function()
 		vim.opt_local.spell = true
+		local wk = require("which-key")
 
 		--  NOTE: VimTex -> Skim -> nvim link
 		-- vim.api.nvim_create_augroup('vimtex_common', {clear = true})
@@ -40,7 +41,36 @@ return {
 		vim.g.vimtex_parser_bib_backend = "lua"
 		vim.g.vimtex_quickfix_open_on_warning = 0 
 		vim.g.vimtex_doc_handlers = {'vimtex#doc#handlers#texdoc'}
-		vim.keymap.set("n", "<localleader>ll", "<Cmd>update<CR><Cmd>VimtexCompile<CR>")
+
+		wk.register({
+			l = {
+				name = "Vimtex",
+			},
+		})
+
+		vim.keymap.set("n", "<localleader>ll", "<Cmd>update<CR><Cmd>VimtexCompile<CR>", {desc = "Vimtex compile"})
+		vim.keymap.set({"n", "x"}, "<localleader>lL", "<Cmd>update<CR><Cmd>VimtexCompileSelected<CR>", {desc = "Vimtex compile selected"})
+		vim.keymap.set("n", "<localleader>li", "<Cmd>VimtexInfo<CR>", {desc = "Vimtex info"})
+		vim.keymap.set("n", "<localleader>lI", "<Cmd>VimtexInfo!<CR>", {desc = "Vimtex info full"})
+		vim.keymap.set("n", "<localleader>lt", "<Cmd>VimtexTocOpen<CR>", {desc = "Vimtex TOC"})
+		vim.keymap.set("n", "<localleader>lT", "<Cmd>VimtexTocToggle<CR>", {desc = "Vimtex toggle TOC"})
+		vim.keymap.set("n", "<localleader>lq", "<Cmd>VimtexLog<CR>", {desc = "Vimtex log"})
+		vim.keymap.set("n", "<localleader>lv", "<Cmd>VimtexView<CR>", {desc = "Vimtex view"})
+		vim.keymap.set("n", "<localleader>lr", "<plug>(Vimtex-reverse-search)", {desc = "Vimtex reverse search"})
+		vim.keymap.set("n", "<localleader>lk", "<Cmd>VimtexStop<CR>", {desc = "Vimtex stop"})
+		vim.keymap.set("n", "<localleader>lK", "<Cmd>VimtexStopAll<CR>", {desc = "Vimtex stop all"})
+		vim.keymap.set("n", "<localleader>le", "<Cmd>VimtexErrors<CR>", {desc = "Vimtex errors"})
+		vim.keymap.set("n", "<localleader>lo", "<Cmd>VimtexCompileOutput<CR>", {desc = "Vimtex compille output"})
+		vim.keymap.set("n", "<localleader>lg", "<Cmd>VimtexStatus<CR>", {desc = "Vimtex status"})
+		vim.keymap.set("n", "<localleader>lG", "<Cmd>VimtexStatus!<CR>", {desc = "Vimtex full status"})
+		vim.keymap.set("n", "<localleader>lc", "<Cmd>VimtexClean<CR>", {desc = "Vimtex clean"})
+		vim.keymap.set("n", "<localleader>lC", "<Cmd>VimtexClean!<CR>", {desc = "Vimtex full clean"})
+		vim.keymap.set("n", "<localleader>lx", "<Cmd>VimtexReload<CR>", {desc = "Vimtex reload"})
+		vim.keymap.set("n", "<localleader>lX", "<Cmd>VimtexReloadState<CR>", {desc = "Vimtex reload state"})
+		vim.keymap.set("n", "<localleader>lm", "<Cmd>VimtexImapsList<CR>", {desc = "Vimtex input maps list"})
+		vim.keymap.set("n", "<localleader>ls", "<Cmd>VimtexToggleMain<CR>", {desc = "Vimtex toggle main"})
+		vim.keymap.set("n", "<localleader>la", "<Cmd>VimtexContextMenu<CR>", {desc = "Vimtex context menu"})
+
 
 		-- VimTeX uses latexmk as the default compiler backend. If you use it, which is
 		-- strongly recommended, you probably don't need to configure anything. If you
