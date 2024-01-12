552cd1fb0c3b89ec36f9798016432b2180e46d11
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-12 17:08:46 +0200

    Add lazy loading

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,19 +4,26 @@ return {
 
 		tag = '0.1.5',
 		cmd = 'Telescope',
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find File" } },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>",  { desc = "Live grep" } },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>",    { desc = "Buffres" } },
+
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>",  { desc = "Help tags" } },
+		},
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 			'nvim-telescope/telescope-ui-select.nvim',
 		},
 
-
 		config = function()
 			local telescope = require("telescope")
+			-- local wk = require("which-key")
 
 			local opts = {
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown{},
+						require("telescope.themes").get_dropdown {},
 					},
 				},
 			}
