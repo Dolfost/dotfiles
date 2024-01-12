dee2ab4e3c9b604a8ddf1648c3f266458e5e1cec
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-12 17:07:42 +0200

    Lazy load telescope and oil.nvim

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -7,14 +7,4 @@ vim.keymap.set('n', '<Left>', '<Nop>')
 vim.keymap.set('n', '<Right>', '<Nop>')
 
 -- Regenerate ctags file with F5
-vim.keymap.set('n', '<f5>', ':!ctags -R<CR>')
-
--- Telescope keybinds
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Telescope find files"})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Telescope live grep"})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Telescope buffers"})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope help tags" })
-
--- oil.nvim keybinds
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set('n', '<f5>', ':!ctags -R<CR>', {desc = "Generate Ctags"})
