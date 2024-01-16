e95a81961bc5eaeed626cc6d973c8b6ae94fb6f7
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-01-16 22:06:42 +0200

    Fix alpha.nvim keybinds

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -27,9 +27,17 @@ return {
 			[[                                                                       ]],
 		}
 
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("<leader>ff", " Fuzzy file search", "<cmd>Telescope find_files<cr>"),
+			dashboard.button("P", "  Plugins", "<cmd>Lazy<cr>"),
+			dashboard.button("S", "  Language servers", "<cmd>Mason<cr>"),
+			dashboard.button("T", "  Loading time", "<cmd>Lazy profile<cr>"),
+			dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+		}
+
 		dashboard.margin = 20
 
 		alpha.setup(dashboard.opts)
 	end,
 }
-
