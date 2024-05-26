07cfa8486b1149beb1f6249ca50dbe65bc7e7db8
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-26 19:50:54 +0300

    Add neotree keybinds

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -9,13 +9,48 @@ return {
 			'MunifTanjim/nui.nvim',
 			'3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
+
 		cmd = 'Neotree',
 
+		init = function ()
+			local wk = require"which-key"
+
+			wk.register({
+				n = {
+					name = "Neotree",
+					l = {"<cmd>Neotree focus left<cr>", "Open on the left"},
+					r = {"<cmd>Neotree focus right<cr>", "Open on the right"},
+					c = {"<cmd>Neotree focus current<cr>", "Open in this buffer"},
+					f = {"<cmd>Neotree focus float<cr>", "Open as float"},
+					t = {
+						name = "Open Neotree and reveal this file",
+						l = {"<cmd>Neotree focus left reveal<cr>", "Open on the left and find this file"},
+						r = {"<cmd>Neotree focus right reveal<cr>", "Open on the right and find this file"},
+						c = {"<cmd>Neotree focus current reveal<cr>", "Open in this buffer and find this file"},
+						f = {"<cmd>Neotree focus float reveal<cr>", "Open as float and find this file"},
+					},
+					g = {
+						name = "Git status",
+						l = {"<cmd>Neotree focus left git_status<cr>", "Open on the left"},
+						r = {"<cmd>Neotree focus right git_status<cr>", "Open on the right"},
+						c = {"<cmd>Neotree focus current git_status<cr>", "Open in this buffer"},
+						f = {"<cmd>Neotree focus float git_status<cr>", "Open as float"},
+					},
+					b = {
+						name = "Buffers",
+						l = {"<cmd>Neotree focus left buffers<cr>", "Open on the left"},
+						r = {"<cmd>Neotree focus right buffers<cr>", "Open on the right"},
+						c = {"<cmd>Neotree focus current buffers<cr>", "Open in this buffer"},
+						f = {"<cmd>Neotree focus float buffers<cr>", "Open as float"},
+					},
+				},
+			}, { prefix = "<leader>" })
+		end,
 
 		opts = {
 			window = {
 				mappings = {
-					["P"] = {"toggle_preview", config = { use_float = false, use_image_nvim = true}},
+					["P"] = {"toggle_preview", config = { use_float = true, use_image_nvim = true}},
 				}
 			},
 
