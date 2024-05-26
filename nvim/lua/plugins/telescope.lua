88d2d15636345e761f2a9af7d7021c1803cd3e1d
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-26 20:20:08 +0300

    Refactor telescope keybinds

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,17 +8,31 @@ return {
 			'Telescope',
 		},
 
-		keys = {
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find File" } },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>",  { desc = "Live grep" } },
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>",    { desc = "Buffres" } },
-			{ "<leader>fh", "<cmd>Telescope help_tags<cr>",  { desc = "Help tags" } },
-		},
-
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 			'nvim-telescope/telescope-ui-select.nvim',
 		},
+
+		init = function ()
+			local wk = require"which-key"
+
+			wk.register({
+				t = {
+					name = "Telescope",
+					f = {"<cmd>Telescope find_files<cr>", "Find File" },
+					g = { "<cmd>Telescope live_grep<cr>", "Live grep" },
+					b = { "<cmd>Telescope buffers<cr>",   "Buffres" },
+					h = { "<cmd>Telescope help_tags<cr>", "Help tags" },
+					s = { "<cmd>Telescope treesitter<cr>", "Treesitter symbols" },
+					o = { "<cmd>Telescope oldfiles<cr>", "List recentry opened files" },
+					c = { "<cmd>Telescope colorscheme<cr>", "List avaliable colorschemes" },
+					m = { "<cmd>Telescope marks<cr>", "List vim marks" },
+					r = { "<cmd>Telescope registers<cr>", "List vim registers and their contents" },
+					j = { "<cmd>Telescope jumplist<cr>", "List vim jumplist" },
+					['/'] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Buffer fuzzy search" },
+				},
+			}, { prefix = "<leader>" })
+		end,
 
 		config = function()
 			local telescope = require("telescope")
@@ -28,6 +42,9 @@ return {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
 					},
+				},
+				defaults = {
+					layout_strategy = "vertical",
 				},
 			}
 
