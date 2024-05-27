e4287db9f066238d3ea852da24494bf145fb6b19
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-28 02:20:20 +0300

    Hugely impvove vimtex setup

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -18,51 +18,97 @@ return {
 		vim.g.vimtex_imaps_enabled = true
 
 		vim.g.vimtex_complete_enabled = true
-		vim.g.vimtex_complete_bib = {
-			menu_fmt = '@key [@type] \'@title\' @author_short (@year)',
-		}
 		vim.g.vimtex_complete_close_braces = true
 		vim.g.vimtex_complete_ignore_case = true
 		vim.g.vimtex_complete_smart_case = true
-		vim.g.vimtex_indent_ignored_env = {}
+		vim.g.vimtex_indent_ignored_envs = {"document"}
+		vim.g.vimtex_complete_bib = {
+			menu_fmt = '[@type] @author_short (@year), "@title"',
+		}
 
-		vim.g.vimtex_syntax_conceal_disable = true
+		vim.g.vimtex_fold_enabled = true
+		vim.g.vimtex_fold_types = {
+			preamble = {enabled = true},
+			sections = {enabled = true},
+		}
 
+		vim.g.vimtex_echo_verbose_input = false
 		vim.g.vimtex_parser_bib_backend = "lua"
 		vim.g.vimtex_quickfix_open_on_warning = false
+
+		vim.g.vimtex_syntax_conceal_disable = false
+		vim.g.vimtex_syntax_conceal = {
+			accents = true,
+			ligatures = true,
+			cites = true,
+			fancy = true,
+			spacing = true,
+			greek = true,
+			math_bounds = true,
+			math_delimiters = true,
+			math_fracs = true,
+			math_super_sub = true,
+			math_symbols = true,
+			sections = true,
+			styles = true,
+		}
+
+    vim.g.vimtex_syntax_conceal_cites = {
+			type = 'brackets',
+			icon = '📖',
+			verbose = true,
+		}
+
+		vim.g.vimtex_doc_confirm_single = false
 		vim.g.vimtex_doc_handlers = { 'vimtex#doc#handlers#texdoc' }
 
-		vim.keymap.set("n", "<localleader>ll", "<Cmd>update<CR><Cmd>VimtexCompile<CR>", { desc = "Vimtex compile" })
-		vim.keymap.set({ "n", "x" }, "<localleader>lL", "<Cmd>update<CR><Cmd>VimtexCompileSelected<CR>",
-			{ desc = "Vimtex compile selected" })
-		vim.keymap.set("n", "<localleader>li", "<Cmd>VimtexInfo<CR>", { desc = "Vimtex info" })
-		vim.keymap.set("n", "<localleader>lI", "<Cmd>VimtexInfo!<CR>", { desc = "Vimtex info full" })
-		vim.keymap.set("n", "<localleader>lt", "<Cmd>VimtexTocOpen<CR>", { desc = "Vimtex TOC" })
-		vim.keymap.set("n", "<localleader>lT", "<Cmd>VimtexTocToggle<CR>", { desc = "Vimtex toggle TOC" })
-		vim.keymap.set("n", "<localleader>lq", "<Cmd>VimtexLog<CR>", { desc = "Vimtex log" })
-		vim.keymap.set("n", "<localleader>lv", "<Cmd>VimtexView<CR>", { desc = "Vimtex view" })
-		vim.keymap.set("n", "<localleader>lr", "<plug>(Vimtex-reverse-search)", { desc = "Vimtex reverse search" })
-		vim.keymap.set("n", "<localleader>lk", "<Cmd>VimtexStop<CR>", { desc = "Vimtex stop" })
-		vim.keymap.set("n", "<localleader>lK", "<Cmd>VimtexStopAll<CR>", { desc = "Vimtex stop all" })
-		vim.keymap.set("n", "<localleader>le", "<Cmd>VimtexErrors<CR>", { desc = "Vimtex errors" })
-		vim.keymap.set("n", "<localleader>lo", "<Cmd>VimtexCompileOutput<CR>", { desc = "Vimtex compille output" })
-		vim.keymap.set("n", "<localleader>lg", "<Cmd>VimtexStatus<CR>", { desc = "Vimtex status" })
-		vim.keymap.set("n", "<localleader>lG", "<Cmd>VimtexStatus!<CR>", { desc = "Vimtex full status" })
-		vim.keymap.set("n", "<localleader>lc", "<Cmd>VimtexClean<CR>", { desc = "Vimtex clean" })
-		vim.keymap.set("n", "<localleader>lC", "<Cmd>VimtexClean!<CR>", { desc = "Vimtex full clean" })
-		vim.keymap.set("n", "<localleader>lx", "<Cmd>VimtexReload<CR>", { desc = "Vimtex reload" })
-		vim.keymap.set("n", "<localleader>lX", "<Cmd>VimtexReloadState<CR>", { desc = "Vimtex reload state" })
-		vim.keymap.set("n", "<localleader>lm", "<Cmd>VimtexImapsList<CR>", { desc = "Vimtex input maps list" })
-		vim.keymap.set("n", "<localleader>ls", "<Cmd>VimtexToggleMain<CR>", { desc = "Vimtex toggle main" })
-		vim.keymap.set("n", "<localleader>la", "<Cmd>VimtexContextMenu<CR>", { desc = "Vimtex context menu" })
+		local wk = require"which-key"
+
+		wk.register({
+			l = {
+				name = "VimTeX",
+				l = { "<cmd>update<cr><cmd>VimtexCompile<cr>", "Compile" },
+				s = { "<cmd>update<cr><cmd>VimtexCompileSS<cr>", "Single shot compile" },
+				L = { "<cmd>update<cr><cmd>VimtexCompileSelected<cr>", "Compile selected"},
+				i = { "<cmd>VimtexInfo<cr>", "Information" },
+				I = { "<cmd>VimtexInfo!<cr>", "Full information" },
+				t = { "<cmd>VimtexTocOpen<cr>", "Table of Contents" },
+				T = { "<cmd>VimtexTocToggle<cr>", "Toggle table of Contents" },
+				q = { "<cmd>VimtexLog<cr>", "Log" },
+				v = { "<cmd>VimtexView<cr>", "View" },
+				r = { "<plug>(Vimtex-reverse-search)", "Reverse search" },
+				k = { "<Cmd>VimtexStop<cr>", "Stop" },
+				K = { "<Cmd>VimtexStopAll<cr>", "Stop all" },
+				e = { "<Cmd>VimtexErrors<cr>", "Errors" },
+				o = { "<Cmd>VimtexCompileOutput<cr>", "Compile output" },
+				g = { "<Cmd>VimtexStatus<cr>", "Status" },
+				G = { "<Cmd>VimtexStatus!<cr>", "Full status" },
+				c = { "<Cmd>VimtexClean<cr>", "Clean" },
+				h = { "<Cmd>VimtexClearCache ALL<cr>", "Clear all cache" },
+				C = { "<Cmd>Vimtexllean!<cr>", "Full clean" },
+				x = { "<Cmd>VimtexReload<cr>", "Reload" },
+				X = { "<Cmd>VimtexReloadState<cr>", "Reload state" },
+				m = { "<Cmd>VimtexImapsList<cr>", "Input mappings" },
+				S = { "<Cmd>VimtexToggleMain<cr>", "Toggle main" },
+				a = { "<Cmd>VimtexContextMenu<cr>", "Context menu"},
+			},
+		}, { prefix = "<leader>" })
+
+		wk.register({
+			l = {
+				name = "VimTex",
+				l = { "<cmd>update<cr><cmd>VimtexCompileSelected<cr>", "Compile selected"},
+			},
+		}, { prefix = "<leader>", mode = "v"})
+
 
 		vim.g.vimtex_compiler_method = 'latexmk'
 		vim.g.vimtex_compiler_latexmk = {
-			build_dir = 'buiddir',
-			callback = 1,
-			continuous = 0,
+			-- auxdir = 'tex_auxiliary',
+			-- out_dir = 'out',
+			callback = true,
+			continuous = true,
 			executable = 'latexmk',
-			hooks = {},
 			options = {
 				'-verbose',
 				'-file-line-error',
@@ -71,5 +117,7 @@ return {
 				'-shell-escape',
 			},
 		}
+
+
 	end,
 }
