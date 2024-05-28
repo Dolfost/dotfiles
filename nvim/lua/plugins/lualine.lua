70abc51f133d6cbb9406964121525b30b081d73c
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-28 18:22:32 +0300

    Set lualine theme to auto

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,11 +3,10 @@ return {
 
 	dependencies = {'nvim-tree/nvim-web-devicons'},
 
-
 	opts = {
 		options = {
 			icons_enabled = true,
-			theme = 'night-owl',
+			theme = 'auto',
 			component_separators = { left = '', right = ''},
 			section_separators = { left = '', right = ''},
 			disabled_filetypes = {
