10210844687bf62206622b9c94aca2f9e3610517
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-29 20:48:56 +0300

    Enable dap and setup the basic C++ debugging

--- nvim/lua/plugins/dapconfig.lua
+++ nvim/lua/plugins/dapconfig.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"jay-babu/mason-nvim-dap.nvim",
 
-		enabled = false,
+		enabled = true,
 		dependencies = {
 			{ "williamboman/mason.nvim", config = true, },
 		},
@@ -11,6 +11,26 @@ return {
 		opts = {
 			ensure_installed = {
 				'python',
+				'codelldb',
+			},
+
+			handlers = {
+				function(config)
+					-- all sources with no handler get passed here
+					-- Keep original functionality
+					require('mason-nvim-dap').default_setup(config)
+				end,
+				-- codelldb = function(config)
+				-- 	config.adapters = {
+				-- 		type = "executable",
+				-- 		command = "/usr/bin/python3",
+				-- 		args = {
+				-- 			"-m",
+				-- 			"debugpy.adapter",
+				-- 		},
+				-- 	}
+				-- 	require('mason-nvim-dap').default_setup(config) -- don't forget this!
+				-- end,
 			},
 		},
 	},
@@ -18,11 +38,86 @@ return {
 	{
 		'mfussenegger/nvim-dap',
 
-		enabled = false,
+		enabled = true,
 		dependencies = {
-			'rcarriga/nvim-dap-ui',
-		},
+			{
+				'rcarriga/nvim-dap-ui',
 
+				opts = {
+					controls = {
+						element = "repl",
+						enabled = true,
+						icons = {
+							disconnect = "",
+							pause = "",
+							play = "",
+							run_last = "",
+							step_back = "",
+							step_into = "",
+							step_out = "",
+							step_over = "",
+							terminate = ""
+						}
+					},
+					element_mappings = {},
+					expand_lines = true,
+					floating = {
+						border = "single",
+						mappings = {
+							close = { "q", "<Esc>" }
+						}
+					},
+					force_buffers = true,
+					icons = {
+						collapsed = "",
+						current_frame = "",
+						expanded = ""
+					},
+					layouts = { {
+						elements = { {
+							id = "scopes",
+							size = 0.25
+						}, {
+							id = "breakpoints",
+							size = 0.25
+						}, {
+							id = "stacks",
+							size = 0.25
+						}, {
+							id = "watches",
+							size = 0.25
+						} },
+						position = "left",
+						size = 40
+					}, {
+						elements = { {
+							id = "repl",
+							size = 0.5
+						}, {
+							id = "console",
+							size = 0.5
+						} },
+						position = "bottom",
+						size = 10
+					} },
+					mappings = {
+						edit = "e",
+						expand = { "<CR>", "<2-LeftMouse>" },
+						open = "o",
+						remove = "d",
+						repl = "r",
+						toggle = "t"
+					},
+					render = {
+						indent = 1,
+						max_value_lines = 100
+					}
+				},
+				dependencies = {
+					'nvim-neotest/nvim-nio',
+				},
+			},
+		},
 
 		config = function(_, opts)
 			local dap, dapui = require("dap"), require("dapui")
@@ -40,83 +135,74 @@ return {
 				dapui.close()
 			end
 
-			vim.keymap.set('n', '<F5>', function() dap.continue() end)
-			vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-			vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-			vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-			vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
-			vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
-			vim.keymap.set('n', '<Leader>lp',
-				function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-			vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-			vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
-			vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-				require('dap.ui.widgets').hover()
-			end)
-			vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-				require('dap.ui.widgets').preview()
-			end)
-			vim.keymap.set('n', '<Leader>df', function()
-				local widgets = require('dap.ui.widgets')
-				widgets.centered_float(widgets.frames)
-			end)
-			vim.keymap.set('n', '<Leader>ds', function()
-				local widgets = require('dap.ui.widgets')
-				widgets.centered_float(widgets.scopes)
-			end)
+			local wk = require("which-key")
 
+			wk.register({
+				name = "Debugger",
+				c = {function() dap.continue() end, "Continue"},
+				s = {
+					name = "Make steps",
+					v = {dap.step_over(), "Step over"},
+					i = {dap.step_into(), "Step into"},
+					o = {dap.step_out(), "Step out"},
+				},
+				b = {function() dap.toggle_breakpoint() end, "Toggle breakpoint"},
+				B = {
+					function()
+						require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+					end, "Set breakpoint with log"
+				},
+				r = {function() dap.repl.open() end, "Open REPL"},
+				l = {function() dap.run_last() end, "Run last"},
+				f = {
+					name = "Floats",
+					f = {
+						function()
+							local widgets = dap.ui.widgets
+							widgets.centered_float(widgets.frames)
+						end,
+						"Frames"
+					},
+					s = {
+						function()
+							local widgets = dap.ui.widgets
+							widgets.centered_float(widgets.scopes)
+						end,
+						"Scopes"
+					},
+				},
+			}, { prefix = "<leader>d", mode = {"n"} })
 
-			dap.configurations.python = {
+			wk.register({
+				h = {function() dap.ui.widgets.hover() end, "Hover"},
+				p = {function() require('dap.ui.widgets').hover() end, "Preview"},
+			}, { prefix = "<leader>d", mode = {"n", "v"} })
+
+			dap.configurations.cpp = {
 				{
-					-- The first three options are required by nvim-dap
-					type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+					name = 'Launch cpp with codelldb',
+					type = 'codelldb',
 					request = 'launch',
-					name = "Launch file",
-
-					-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-					program = "${file}", -- This configuration will launch the current file if used.
-					pythonPath = function()
-						-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-						-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-						-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-						local cwd = vim.fn.getcwd()
-						if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-							return cwd .. '/venv/bin/python'
-						elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-							return cwd .. '/.venv/bin/python'
-						else
-							return '/usr/bin/python'
-						end
+					program = function()
+						return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
 					end,
+					cwd = '${workspaceFolder}',
+					stopOnEntry = false,
+					args = {},
+
+					-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+					--
+					--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+					--
+					-- Otherwise you might get the following error:
+					--
+					--    Error on launch: Failed to attach to the target process
+					--
+					-- But you should be aware of the implications:
+					-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+					-- runInTerminal = false,
 				},
 			}
-
-			dap.adapters.python = function(cb, config)
-				if config.request == 'attach' then
-					---@diagnostic disable-next-line: undefined-field
-					local port = (config.connect or config).port
-					---@diagnostic disable-next-line: undefined-field
-					local host = (config.connect or config).host or '127.0.0.1'
-					cb({
-						type = 'server',
-						port = assert(port, '`connect.port` is required for a python `attach` configuration'),
-						host = host,
-						options = {
-							source_filetype = 'python',
-						},
-					})
-				else
-					cb({
-						type = 'executable',
-						command = '/Users/vladyslav/.local/share/nvim/mason/packages/debugpy/debugpy',
-						args = { '-m', 'debugpy.adapter' },
-						options = {
-							source_filetype = 'python',
-						},
-					})
-				end
-			end
 		end
 	},
 }
