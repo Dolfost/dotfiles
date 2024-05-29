95783428932949f54be4faba083474bfb55a44d7
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-29 12:42:50 +0300

    Move to lua gruvbox colorscheme

--- nvim/lua/plugins/colorschemes/gruvbox.lua
+++ nvim/lua/plugins/colorschemes/gruvbox.lua
@@ -1,11 +1,32 @@
 return {
-	'morhetz/gruvbox',
-
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000 ,
 	lazy = true,
-	priority = 1000,
+	config = true,
 
-	config = function()
-		vim.g.gruvbox_italic = 1
-		vim.g.gruvbox_contrast_dark = "hard"
-	end,
+	opts = {
+		terminal_colors = true, -- add neovim terminal colors
+		undercurl = true,
+		underline = true,
+		bold = true,
+
+		italic = {
+			strings = false,
+			emphasis = true,
+			comments = true,
+			operators = true,
+			folds = true,
+		},
+		strikethrough = true,
+		invert_selection = false,
+		invert_signs = false,
+		invert_tabline = false,
+		invert_intend_guides = false,
+		inverse = true, -- invert background for search, diffs, statuslines and errors
+		contrast = "hard", -- can be "hard", "soft" or empty string
+		palette_overrides = {},
+		overrides = {},
+		dim_inactive = false,
+		transparent_mode = false,
+	}
 }
