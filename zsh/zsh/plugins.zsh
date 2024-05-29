cea4b58b3ed1243e7e88cea9fd641493272bab43
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-29 10:58:06 +0300

    Change theme

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -2,8 +2,8 @@
 
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions"
-# zplug "zsh-users/zsh-completions"
-zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
+zplug "subnixr/minimal", as:theme
+# zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
 zplug "plugins/sudo", from:oh-my-zsh
 zplug "plugins/web-search", from:oh-my-zsh
 zplug "plugins/dirhistory", from:oh-my-zsh
