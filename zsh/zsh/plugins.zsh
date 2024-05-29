80f463fe49376028e0db94939c4cc9f3d00f7049
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-29 10:49:29 +0300

    Remove unnecesarry plugins

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -1,11 +1,8 @@
 # Plugins (zplug)
 
-zplug "plugins/git", from:oh-my-zsh
-# zplug "catppuccin/zsh-syntax-highlighting", hook-load:"source $ZPLUG_HOME/repos/catppuccin/zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh" #  NOTE: good solution, bad themes
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-completions"
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#616161, bg=None, bold, underline"
+# zplug "zsh-users/zsh-completions"
 zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
 zplug "plugins/sudo", from:oh-my-zsh
 zplug "plugins/web-search", from:oh-my-zsh
