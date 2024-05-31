cd713811c430d2624db9aaf33f8136978e4cba39
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-31 21:46:16 +0300

    Add treesitter text objects plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -38,8 +38,56 @@ return {
 				additional_vim_regex_highlighting = false,
 			},
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
+	},
+
+	{
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		dependencies = {'nvim-treesitter/nvim-treesitter'},
+
+		config = function ()
+---@diagnostic disable-next-line: missing-fields
+			require"nvim-treesitter.configs".setup{
+				textobjects = {
+					select = {
+						enable = true,
+
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = {query = "@function.outer", desc = "function"},
+							["if"] = {query = "@function.inner", desc = "function"},
+							["ac"] = {query = "@class.outer", desc = "class"},
+							["ic"] = { query = "@class.inner", desc = "class" },
+							-- You can also use captures from other query groups like `locals.scm`
+							["as"] = { query = "@scope", query_group = "locals", desc = "scope" },
+						},
+						-- You can choose the select mode (default is charwise 'v')
+						--
+						-- Can also be a function which gets passed a table with the keys
+						-- * query_string: eg '@function.inner'
+						-- * method: eg 'v' or 'o'
+						-- and should return the mode ('v', 'V', or '<c-v>') or a table
+						-- mapping query_strings to modes.
+						selection_modes = {
+							['@parameter.outer'] = 'v', -- charwise
+							['@function.outer'] = 'V', -- linewise
+							['@class.outer'] = '<c-v>', -- blockwise
+						},
+						-- If you set this to `true` (default is `false`) then any textobject is
+						-- extended to include preceding or succeeding whitespace. Succeeding
+						-- whitespace has priority in order to act similarly to eg the built-in
+						-- `ap`.
+						--
+						-- Can also be a function which gets passed a table with the keys
+						-- * query_string: eg '@function.inner'
+						-- * selection_mode: eg 'v'
+						-- and should return true or false
+						include_surrounding_whitespace = true,
+					},
+				},
+			}
 		end,
 	},
 }
