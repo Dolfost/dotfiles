07be6b0580bf26639d9beace8326ff23edf03918
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-06-01 13:48:53 +0300

    Change step size

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -74,10 +74,10 @@ return {
 				enable_default_keybindings = true,
 
 				-- sets resize steps for x axis
-				resize_step_x = 2,
+				resize_step_x = 1,
 
 				-- sets resize steps for y axis
-				resize_step_y = 4,
+				resize_step_y = 1,
 			}
 		}
 	},
