f5a25021b5c9052398b8b2c2812713d9f852f072
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-06-04 22:10:38 +0300

    Add netman support

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -54,6 +54,50 @@ return {
 				}
 			},
 
+			sources = {
+				'filesystem',
+				"netman.ui.neo-tree",
+				"buffers",
+				"git_status",
+			},
+
+      source_selector = {
+        winbar = false, -- toggle to show selector on winbar
+        statusline = false, -- toggle to show selector on statusline
+        show_scrolled_off_parent_node = false,                    -- boolean
+        sources = {                                               -- table
+          {
+            source = "filesystem",                                -- string
+            display_name = " 󰉓 Files "                            -- string | nil
+          },
+          {
+            source = "buffers",                                   -- string
+            display_name = " 󰈚 Buffers "                          -- string | nil
+          },
+          {
+            source = "git_status",                                -- string
+            display_name = " 󰊢 Git "                              -- string | nil
+          },
+					{
+						source = "remote",
+					},
+        },
+        content_layout = "start",                                 -- string
+        tabs_layout = "equal",                                    -- string
+        truncation_character = "…",                               -- string
+        tabs_min_width = nil,                                     -- int | nil
+        tabs_max_width = nil,                                     -- int | nil
+        padding = 0,                                              -- int | { left: int, right: int }
+        separator = { left = "▏", right= "▕" },                   -- string | { left: string, right: string, override: string | nil }
+        separator_active = nil,                                   -- string | { left: string, right: string, override: string | nil } | nil
+        show_separator_on_edge = false,                           -- boolean
+        highlight_tab = "NeoTreeTabInactive",                     -- string
+        highlight_tab_active = "NeoTreeTabActive",                -- string
+        highlight_background = "NeoTreeTabInactive",              -- string
+        highlight_separator = "NeoTreeTabSeparatorInactive",      -- string
+        highlight_separator_active = "NeoTreeTabSeparatorActive", -- string
+      },
+
 			default_component_configs = {
 				git_status = {
 					symbols = {
---
+++ nvim/lua/plugins/netman.lua
@@ -0,0 +1,7 @@
+-- Add this to your Lazy plugin setup section
+return {
+	'miversen33/netman.nvim',
+	-- Note, you do not need this if you plan on using Netman with any of the
+	-- supported UI Tools such as Neo-tree
+	config = true
+}
