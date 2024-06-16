56910f1d06305045042dec78b38b7ed5981eaf12
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-06-17 01:24:36 +0300

    Compress darwin conditionals

--- zsh/zsh/exports.zsh
+++ zsh/zsh/exports.zsh
@@ -5,24 +5,19 @@ export LANG=en_US.UTF-8
 
 # cargo
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	export PATH="$PATH:/Users/vladyslav/.cargo/bin"
+	export PATH="$PATH:$HOME/.cargo/bin"
+	# Add libgdiplus to csc library path on mac
+	export DYLD_LIBRARY_PATH="/opt/homebrew/Cellar/mono-libgdiplus/6.1_2/lib/"
+	export ZPLUG_HOME=/opt/homebrew/opt/zplug
+else
+	export ZPLUG_HOME=~/.zplug
 fi
 
 # My sctipts
 export PATH="$PATH:$HOME/.scripts"
 
-# Add libgdiplus to csc library path on mac
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	export DYLD_LIBRARY_PATH="/opt/homebrew/Cellar/mono-libgdiplus/6.1_2/lib/"
-fi
-
 # Remove files in .gitignore from fzf list 
 export FZF_DEFAULT_COMMAND='rg --files'
 
 # zplug
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	export ZPLUG_HOME=/opt/homebrew/opt/zplug
-else
-	export ZPLUG_HOME=~/.zplug
-fi
 source $ZPLUG_HOME/init.zsh
