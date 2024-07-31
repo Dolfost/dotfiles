680ad1e5037114a34717d37ff749c5593db25ec1
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-07-31 13:51:45 +0300

    Fix tresitter options

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
@@ -5,41 +6,46 @@ return {
 		lazy = true,
 		enabled = true,
 
-		opts = {
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = {'c', 'cpp', 'bash', 'python', 'c_sharp', 'git_rebase', 'git_config', 'gitignore', 
-			'luadoc', 'vimdoc', 'lua', 'vim',},
+		config = function()
+			local configs = require"nvim-treesitter.configs"
+			configs.setup({
+				-- A list of parser names, or "all" (the five listed parsers should always be installed)
+				ensure_installed = {'c', 'cpp', 'bash', 'python', 'c_sharp', 'git_rebase', 'git_config', 'gitignore',
+				'vimdoc', 'lua', 'vim', 'query', 'markdown', 'markdown_inline'},
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = true,
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = true,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
 
-			-- List of parsers to ignore installing (for "all")
-			ignore_install = { "latex" },
+				-- List of parsers to ignore installing (for "all")
+				ignore_install = { "latex" },
 
-			-- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+				-- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-			highlight = {
-				enable = true,
+				highlight = {
+					enable = true,
+					disable = { 'markdown', 'markdown_inline' };
 
-				-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-				-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-				-- the name of the parser)
-				-- list of language that will be disabled
-				disable = {'latex', 'gitcommit', 'markdown'},
-				-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+					-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+					-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+					-- the name of the parser)
+					-- list of language that will be disabled
+					-- disable = {'latex', 'gitcommit', 'markdown'},
+					-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-		},
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true }
+			})
+		end
 	},
 
 	{
