3c7dcdee0dd071b915cadf379785734c7e9704ba
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-08-01 14:29:38 +0300

    Replace end of buffer symbol with space

--- nvim/lua/config/visuals.lua
+++ nvim/lua/config/visuals.lua
@@ -1,4 +1,5 @@
 vim.cmd.colorscheme("night-owl")
+vim.opt.fillchars = 'eob: '
 
 -- vim.cmd([[highlight DiagnosticError gui=NONE guibg=NONE guifg=Red]])
 -- vim.cmd([[highlight DiagnosticWarning gui=NONE guibg=NONE guifg=Orange]])
