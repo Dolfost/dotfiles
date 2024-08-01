84a2834d7eb87f83b3333ead0a7e111d13cd66ec
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-08-01 11:28:17 +0300

    Add new keybinds

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,10 +1,39 @@
 -- This is a global keybinds file. The plugin-specific keybinds live in lua/plugin.lua or ftplugin/
 
+local wk = require"which-key"
+
 -- NO ARROW KEYS, U MFCKER! heheheha!!1!
-vim.keymap.set('n', '<Up>', '<Nop>')
-vim.keymap.set('n', '<Down>', '<Nop>')
-vim.keymap.set('n', '<Left>', '<Nop>')
-vim.keymap.set('n', '<Right>', '<Nop>')
+wk.add{
+	mode = { "n", "v" },
+	{ '<Up>',    '<Nop>', hidden = true },
+	{ '<Down>',  '<Nop>', hidden = true },
+	{ '<Left>',  '<Nop>', hidden = true },
+	{ '<Right>', '<Nop>', hidden = true },
+}
+
+-- some good shortcuts
+wk.add{
+	mode = { "n", "v" },
+	{ "<leader>q", "<cmd>q<cr>", desc = "Quit",
+		icon = {icon = '', color = 'red'},
+	},
+	{ "<leader>w", "<cmd>w<cr>", desc = "Write",
+		icon = {icon = '', color = 'green'},
+	},
+}
+
+wk.add{
+	mode = { "n", "v" },
+	{ "<leader>M", group = "Managers",
+		icon = {icon = '', color = 'cyan'},
+	},
+	{ "<leader>Ml", "<cmd>Lazy<cr>", desc = "Lazy",
+		icon = {icon = '󰒲', color = 'purple'},
+	},
+	{ "<leader>Mm", "<cmd>Mason<cr>", desc = "Mason",
+		icon = {icon = '', color = 'purple'},
+	}
+}
 
 -- Regenerate ctags file with F5
 -- vim.keymap.set('n', '<f5>', ':!ctags -R<CR>', {desc = "Generate Ctags"})
