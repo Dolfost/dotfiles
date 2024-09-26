ed9cacfb2947fd3812a1a198544a99c54dcc4c53
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-09-26 19:55:00 +0300

    Add limit to lsp suggestion windows size

--- nvim/lua/config/visuals.lua
+++ nvim/lua/config/visuals.lua
@@ -1,5 +1,6 @@
 vim.cmd.colorscheme("night-owl")
 vim.opt.fillchars = 'eob: '
+vim.opt.pumheight = 30
 
 -- vim.cmd([[highlight DiagnosticError gui=NONE guibg=NONE guifg=Red]])
 -- vim.cmd([[highlight DiagnosticWarning gui=NONE guibg=NONE guifg=Orange]])
