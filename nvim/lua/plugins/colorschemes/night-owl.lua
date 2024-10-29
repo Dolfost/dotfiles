a566a84e9974d676978962243f76ffeafeafe655
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-10-29 19:48:42 +0200

    Remove trailing space

--- nvim/lua/plugins/colorschemes/night-owl.lua
+++ nvim/lua/plugins/colorschemes/night-owl.lua
@@ -1,4 +1,4 @@
-return { 
+return {
 	"oxfist/night-owl.nvim",
 	lazy = true, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
