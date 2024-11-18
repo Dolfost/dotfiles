3075a0a57a33b155e494284d3a030e47bd7396e3
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-11-18 22:25:46 +0200

    Add case-insensetive completion to zsh

--- zsh/zsh/visuals.zsh
+++ zsh/zsh/visuals.zsh
@@ -2,4 +2,10 @@ eval `dircolors $ZSH_CONFIG/dir_colors`
 
 zmodload zsh/complist
 
+# use LSCOLORS for tab completion
 zstyle ':completion:*:default' list-colors ${(s.:.)LSCOLORS}
+
+# case insensitive path-completion
+autoload -Uz +X compinit && compinit
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*' menu select
