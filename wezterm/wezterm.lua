9465cfbd4d63d53e6f0a689aa955aec593885407
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-11-20 00:47:19 +0200

    Turn off window resizion on font size change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,6 +18,7 @@ c.native_macos_fullscreen_mode = true
 
 c.font = w.font("Iosevka Nerd Font", { weight = 'Light'})
 c.font_size = 18
+c.adjust_window_size_when_changing_font_size = false
 c.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
 c.scrollback_lines = 0
