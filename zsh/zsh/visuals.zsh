c5b1e558fd8a1ff1056ff2c3d1cf576c92ca4b93
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-11-19 19:32:47 +0200

    Add minimal theme configuration

--- zsh/zsh/visuals.zsh
+++ zsh/zsh/visuals.zsh
@@ -9,3 +9,9 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 autoload -Uz +X compinit && compinit
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 zstyle ':completion:*' menu select
+
+# minimal theme setup
+export MNML_PROMPT=(mnml_ssh mnml_pyenv mnml_status mnml_keymap)
+export MNML_RPROMPT=('mnml_cwd 3 0' mnml_git)
+export MNML_INFOLN=(mnml_err mnml_jobs mnml_uhp mnml_files)
+export MNML_MAGICENTER=(mnml_me_dirs mnml_me_ls mnml_me_git)
