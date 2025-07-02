7e1c574ca2f5aca506f4da9acf0cb7424b52f673
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-07-02 14:12:52 +0300

    Remove unnecesary comment

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -110,7 +110,6 @@ return {
 		dependencies = {'nvim-treesitter/nvim-treesitter'},
 
 		config = function ()
-			---@diagnostic disable-next-line: missing-fields
 			require"nvim-treesitter.configs".setup{
 				refactor = {
 					highlight_definitions = {
