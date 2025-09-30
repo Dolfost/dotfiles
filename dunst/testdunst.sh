682a8180845bd08e7e42e0ec74f6c20017eec65c
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-09-30 18:00:17 +0300

    Configure dunst

--- dunst/testdunst.sh
+++ dunst/testdunst.sh
@@ -1,6 +1,5 @@
 #!/bin/env sh
 
-notify-send -h string:fgcolor:#ff4444 "Message 1"
-notify-send -h string:bgcolor:#4444ff -h string:fgcolor:#ff4444 -h string:frcolor:#44ff44 "Message 2"
 notify-send -h int:value:42 "Working ..."
 notify-send -i info "Information" "This is an informational message."
+notify-send -i info "Information" "Mom, I am in a notification"
