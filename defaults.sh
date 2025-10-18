549eecb980c25b0e6a244fbea2b13df8585c1bfa
Vladyslav <rehanvladyslav@gmail.com>
2025-10-18 22:59:34 +0300

    Add yabai specific settings

--- defaults.sh
+++ defaults.sh
@@ -3,3 +3,10 @@
 # details: man defaults
 
 defaults write com.apple.screencapture "disable-shadow" -bool "true"
+
+
+# Defaults for yabai
+defaults write com.apple.finder DisableAllAnimations -bool true
+killall Finder # or logout and login
+# to reset system defaults, delete the key instead
+# defaults delete com.apple.finder DisableAllAnimations
