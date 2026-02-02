4e2fd18ebf900040689f2ed4dc203c1328940c1e
Vladyslav <rehanvladyslav@gmail.com>
2026-02-02 16:13:11 +0200

    Remove textobjects and make tresitter config more consistent

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,9 +6,7 @@ return {
 		lazy = true,
 		enabled = true,
 
-		config = function()
-			local configs = require"nvim-treesitter.configs"
-			configs.setup({
+		config = {
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
 				ensure_installed = {'c', 'cpp', 'bash', 'python', 'c_sharp', 'git_rebase', 'git_config', 'gitignore',
 					'doxygen',
@@ -49,76 +47,6 @@ return {
 				indent = { enable = true,
 					disable = { "latex", 'bibtex' },
 				}
-			})
-		end
-	},
-
-	{
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		dependencies = {'nvim-treesitter/nvim-treesitter'},
-
-		config = function ()
-			---@diagnostic disable-next-line: missing-fields
-			require"nvim-treesitter.configs".setup{
-				textobjects = {
-					select = {
-						enable = true,
-
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
-
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["af"] = {query = "@function.outer", desc = "function"},
-							["if"] = {query = "@function.inner", desc = "function"},
-							["ac"] = {query = "@class.outer", desc = "class"},
-							["ic"] = { query = "@class.inner", desc = "class" },
-							-- You can also use captures from other query groups like `locals.scm`
-							["as"] = { query = "@scope", query_group = "locals", desc = "scope" },
-						},
-						-- You can choose the select mode (default is charwise 'v')
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * method: eg 'v' or 'o'
-						-- and should return the mode ('v', 'V', or '<c-v>') or a table
-						-- mapping query_strings to modes.
-						selection_modes = {
-							['@parameter.outer'] = 'v', -- charwise
-							['@function.outer'] = 'V', -- linewise
-							['@class.outer'] = '<c-v>', -- blockwise
-						},
-						-- If you set this to `true` (default is `false`) then any textobject is
-						-- extended to include preceding or succeeding whitespace. Succeeding
-						-- whitespace has priority in order to act similarly to eg the built-in
-						-- `ap`.
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * selection_mode: eg 'v'
-						-- and should return true or false
-						include_surrounding_whitespace = true,
-					},
-				},
 			}
-		end,
 	},
-
-	-- { -- super processor hungry on big files (tested on 10k lines)
-	-- 	'nvim-treesitter/nvim-treesitter-refactor',
-	--
-	-- 	dependencies = {'nvim-treesitter/nvim-treesitter'},
-	--
-	-- 	config = function ()
-	-- 		require"nvim-treesitter.configs".setup{
-	-- 			refactor = {
-	-- 				highlight_definitions = {
-	-- 					enable = true,
-	-- 					-- Set to false if you have an `updatetime` of ~100.
-	-- 					clear_on_cursor_move = true,
-	-- 				},
-	-- 			},
-	-- 		}
-	-- 	end,
-	-- },
 }
