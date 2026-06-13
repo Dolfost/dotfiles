abd167b4ddb58f57af470f0d5e2b0f1864611828
Vladyslav Rehan <rehanvladyslav@gmail.com>
2026-06-14 00:45:07 +0300

    Add wrappers

--- hypr/hypr/autostart.lua
+++ hypr/hypr/autostart.lua
@@ -2,17 +2,21 @@ function exec_cmd_uwsm(cmd, opts)
 	return hl.exec_cmd('uwsm app -p After=waybar.service -- ' .. cmd, opts)
 end
 
+function start_systemd_service(service, opts)
+	return hl.exec_cmd('systemctl --user ' .. (opts or '') .. ' start ' .. service)
+end
+
 hl.on("hyprland.start", function()
 	hl.exec_cmd('hyprlock') --  WARN: IMPORTANT
 
 	hl.exec_cmd('systemctl --user start waybar.service')
 
-	hl.exec_cmd('systemctl --user start waybar.service')
-	hl.exec_cmd('systemctl --user start hypridle')
-	hl.exec_cmd('systemctl --user start hyprpaper')
-	hl.exec_cmd('systemctl --user start hyprpolkitagent')
-	hl.exec_cmd('systemctl --user start hyprsunset')
-	hl.exec_cmd('systemctl --user start dunst')
+	start_systemd_service('waybar.service')
+	start_systemd_service('hypridle')
+	start_systemd_service('hyprpaper')
+	start_systemd_service('hyprpolkitagent')
+	start_systemd_service('hyprsunset')
+	start_systemd_service('dunst')
 	hl.exec_cmd('wl-paste --type text  --watch cliphist store')
 	hl.exec_cmd('wl-paste --type image --watch cliphist store')
 
@@ -20,7 +24,7 @@ hl.on("hyprland.start", function()
 	exec_cmd_uwsm(WEB_BROWSER, { workspace = '2 silent' })
 
 	exec_cmd_uwsm('openrgb -p ~/.config/OpenRGB/OFF.orp')
-	exec_cmd_uwsm('sleep 10s; easyeffects --hide-window --service-mode')
+	exec_cmd_uwsm('easyeffects --hide-window --service-mode')
 	exec_cmd_uwsm('signal-desktop')
 	exec_cmd_uwsm('element-desktop --hidden')
 	exec_cmd_uwsm('Telegram -startintray')
