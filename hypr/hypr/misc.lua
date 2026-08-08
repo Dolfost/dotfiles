5d5f8d37dd793c5ed6b0b486913ba4a2de694f1c
Vladyslav Rehan <rehanvladyslav@gmail.com>
2026-08-08 17:35:36 +0300

    Add vrr setting

--- hypr/hypr/misc.lua
+++ hypr/hypr/misc.lua
@@ -2,6 +2,7 @@ hl.config({
 	misc = {
 		disable_hyprland_logo = true,
 		disable_splash_rendering = false,
+		vrr = 1, -- 1=always on. Needed for gamescope (nested) VRR; vrr=2 (fullscreen-only) does NOT engage for gamescope windows.
 	}
 })
 
