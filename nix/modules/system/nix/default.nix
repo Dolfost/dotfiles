a7a2111116871ac6390ea7fa6aa72caa6d6b7792
Vladyslav Rehan <rehanvladyslav@gmail.com>
2026-09-04 17:35:56 +0300

    Make the flake.nix link to /etc/nixos

--- nix/modules/system/nix/default.nix
+++ nix/modules/system/nix/default.nix
@@ -1,4 +1,11 @@
-{ ... }: {
+{ config, ... }: {
 	nixpkgs.config.allowUnfree = true;
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+	# Lets plain `nixos-rebuild switch` find the flake: it defaults to
+	# /etc/nixos/flake.nix#<hostname> when that file exists. The source is a
+	# string on purpose - a path literal would be copied into the store, a string
+	# stays a symlink to the live checkout.
+	environment.etc."nixos/flake.nix".source =
+		"${config.users.users.${config.dotfiles.user}.home}/dotfiles/flake.nix";
 }
