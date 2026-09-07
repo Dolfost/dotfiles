ea2d42333500c68e9367c5cd9d4aa15dfdc5bbf5
Vladyslav Rehan <rehanvladyslav@gmail.com>
2026-09-07 21:55:01 +0300

    Update locale and configure ashell a bit

--- nix/modules/system/locale/default.nix
+++ nix/modules/system/locale/default.nix
@@ -1,4 +1,8 @@
 { ... }: {
 	time.timeZone = "Europe/Kyiv";
 	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_TIME = "en_GB.UTF-8";
+		LC_MEASUREMENT = "uk_UA.UTF-8";
+	};
 }
